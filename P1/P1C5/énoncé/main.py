77ce14b0be81ba4762a39aa8410ed3f03755b012
khalidaitali1 <khalid.aitali@sorbonne.ae>
2026-08-08 18:20:09 +0400

    Update main.py

--- P1/P1C5/énoncé/main.py
+++ P1/P1C5/énoncé/main.py
@@ -1,1 +1,6 @@
-# Écrivez votre code ici !
+# nom="khalid"
+# age=27
+# taille=1.80
+# est_etudiant=False
+# print(f"bonjour ,je m'appelle {nom}, j'ai {age}, je fais {taille} et {est_etudiant} je suis pas etudiant")
+# type(nom)
